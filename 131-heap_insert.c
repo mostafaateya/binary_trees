81e048522f8aeace9cbdc5fe9f6c3cc3d3a3bc96
mostafaateya <mostafaateya@gmail.com>
2023-08-31 19:53:23 -0700

    39. Heap - Extract

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,6 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *xxx;
+
+	xxx = malloc(sizeof(binary_tree_t));
+	if (xxx == NULL)
+		return (NULL);
+
+	xxx->n = value;
+	xxx->parent = parent;
+	xxx->left = NULL;
+	xxx->right = NULL;
+
+	return (xxx);
+}
+
+/**
  * heap_insert - inserts a value in Max Binary Heap
  * @root: a double pointer to the root node of the Heap to insert the value
  * @value: the value to store in the node to be inserted
